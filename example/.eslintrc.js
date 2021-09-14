3afbdf69377cecc5a8b0607360478d7b9a0f0b8c
Santiago Molina Orozco <santi020k@gmail.com>
2021-09-14 16:30:36 -0500

    🚑 HOTFIX: Fix example eslint

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-  "extends": [
-    "react-app",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb-typescript-prettier"
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb-typescript-prettier',
   ],
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
-  "parserOptions": {
-    "project": "./tsconfig.json"
-  }
-}
+  ignorePatterns: ['example/**'],
+  parserOptions: {
+    project: './tsconfig.json',
+    createDefaultProgram: true,
+  },
+};
