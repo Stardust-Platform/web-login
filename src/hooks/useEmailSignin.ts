aa0a334974bc90e8eea200657d65b855b2765ae4
David Giraldo <davidgiraldobueno@gmail.com>
2021-10-07 16:46:29 -0500

    🔨 REFACTOR: Magic link Logic Implementation, Front

---
+++ src/hooks/useEmailSignin.ts
@@ -0,0 +1,83 @@
+// libs
+import axios from 'axios';
+import { Auth } from 'aws-amplify';
+// Interfaces
+import { EmailError } from '../screens/Signin/types';
+
+const loginUrl = 'https://zgdyt9fjia.execute-api.us-east-1.amazonaws.com/dev/login';
+
+const emailRegex = new RegExp(/^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+type UseEmailSigninProps = {
+  email: string;
+  setIsEmailLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setEmailError: React.Dispatch<React.SetStateAction<EmailError>>;
+  isSingup: boolean;
+};
+
+const useEmailSignin = (
+  {
+    email, setIsEmailLoading, setEmailError, isSingup,
+  }: UseEmailSigninProps,
+) => {
+  const cleanErrors = () => {
+    setEmailError({
+      hasError: false, message: '',
+    });
+  };
+
+  const loginWithMagicLink = async () => {
+    try {
+      await axios.post(loginUrl, { email });
+      cleanErrors();
+      setIsEmailLoading(true);
+    } catch {
+      setEmailError({
+        hasError: false, message: 'There was an error. Try again.',
+      });
+    }
+  };
+
+  const SigninSignupWithEmail = async () => {
+    if (email.length === 0 || !(emailRegex.test(email))) {
+      setEmailError({
+        hasError: true, message: 'Enter a valid email.',
+      });
+      return;
+    }
+
+    if (isSingup) {
+      if (typeof window === 'undefined') return;
+      const array = new Uint32Array(5);
+      crypto.getRandomValues(array);
+
+      try {
+        await Auth.signUp({
+          username: email,
+          password: array.join('-'),
+          attributes: {
+            email,
+          },
+        });
+        cleanErrors();
+        setIsEmailLoading(true);
+        loginWithMagicLink();
+      } catch (error: any) {
+        setEmailError({
+          hasError: true, message: error.message,
+        });
+      }
+    } else if (!isSingup) {
+      loginWithMagicLink();
+    }
+  };
+
+  return {
+    loginWithMagicLink,
+    SigninSignupWithEmail,
+    cleanErrors,
+    emailRegex,
+  };
+};
+
+export default useEmailSignin;
