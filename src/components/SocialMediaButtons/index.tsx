6afcc0552e1dffb4e7d0af8590577c3dd67b6b42
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2021-12-06 16:41:06 +0200

    disabled 2 buttons

--- src/components/SocialMediaButtons/index.tsx
+++ src/components/SocialMediaButtons/index.tsx
@@ -5,7 +5,7 @@ import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 // Components
 import Icon, { IconsEnum } from '../Icons';
 // Styles
-import { SocialMediaButton, IconContainer } from './styles';
+import { SocialMediaButton, IconContainer, Disable } from './styles';
 
 const SocialMediaButtons = () => (
   <>
@@ -24,6 +24,19 @@ const SocialMediaButtons = () => (
     <SocialMediaButton
       type="button"
       onClick={() => Auth.federatedSignIn({
+        customProvider: 'Discord',
+      })}
+    >
+      <IconContainer>
+        <Icon icon={IconsEnum.Discord} />
+      </IconContainer>
+      Continue with Discord
+    </SocialMediaButton>
+
+    <Disable>
+    <SocialMediaButton
+      type="button"
+      onClick={() => Auth.federatedSignIn({
         provider: CognitoHostedUIIdentityProvider.Facebook,
       })}
     >
@@ -32,7 +45,9 @@ const SocialMediaButtons = () => (
       </IconContainer>
       Continue with Facebook
     </SocialMediaButton>
+    </Disable>
 
+    <Disable>
     <SocialMediaButton
       type="button"
       onClick={() => Auth.federatedSignIn({
@@ -44,18 +59,8 @@ const SocialMediaButtons = () => (
       </IconContainer>
       Continue with Apple
     </SocialMediaButton>
+    </Disable>
 
-    <SocialMediaButton
-      type="button"
-      onClick={() => Auth.federatedSignIn({
-        customProvider: 'Discord',
-      })}
-    >
-      <IconContainer>
-        <Icon icon={IconsEnum.Discord} />
-      </IconContainer>
-      Continue with Discord
-    </SocialMediaButton>
   </>
 );
 
