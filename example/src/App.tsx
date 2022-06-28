c036382c9aa9ddf2adbb30c913b475cd36b2da76
Vance Ingalls <vanceingalls@Vances-MBP.lan>
2022-06-28 10:27:47 -0700

    update example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,14 +8,28 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import webLoginDevAmplifyConfig from './config/web-login-amplify-config.dev';
+
+let loginUrl: typeof process.env.REACT_APP_WEB_LOGIN_URL;
+let awsOverwrite: typeof webLoginDevAmplifyConfig;
+
+if (process.env.REACT_APP_TARGET_ENVIRONMENT === 'dev') {
+  loginUrl = process.env.REACT_APP_WEB_LOGIN_URL;
+  awsOverwrite = webLoginDevAmplifyConfig;
+}
 
 const App: FC = () => (
-  <AuthProvider isOpen custom={{ privacyPolicyUrl: 'localhost:3000' }}>
-  <BrowserRouter>
+  <AuthProvider
+    isOpen
+    custom={{ privacyPolicyUrl: 'localhost:3000' }}
+    awsOverwrite={awsOverwrite}
+    loginUrl={loginUrl}
+  >
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainScreen/>} />
+        <Route path="/" element={<MainScreen />} />
       </Routes>
-  </BrowserRouter>
+    </BrowserRouter>
   </AuthProvider>
 );
 
