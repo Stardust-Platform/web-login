3fdd5ff8528521205192edc1c8b8f889b75f8a61
Vance Ingalls <vanceingalls@gmail.com>
2022-06-28 11:55:03 -0700

    refactor aws config overwrite (#81)
    * refactor aws config overwrite
    
    On page reloads (redirects) config was not overwritten in time
    Deprecate overwriteEnvironment method
    Add awsConfig and loginUrl props to AuthProvider component
    
    version bump
    
    Co-authored-by: Vance Ingalls <vanceingalls@vances-mbp.lan>

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,14 +8,29 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import webLoginDevAmplifyConfig from './config/web-login-amplify-config.dev';
+
+const devProps: {
+  loginUrl?: typeof process.env.REACT_APP_WEB_LOGIN_URL,
+  awsOverwrite?: typeof webLoginDevAmplifyConfig
+} = {}
+
+if (process.env.REACT_APP_TARGET_ENVIRONMENT === 'dev') {
+  devProps.loginUrl = process.env.REACT_APP_WEB_LOGIN_URL;
+  devProps.awsOverwrite = webLoginDevAmplifyConfig;
+}
 
 const App: FC = () => (
-  <AuthProvider isOpen custom={{ privacyPolicyUrl: 'localhost:3000' }}>
-  <BrowserRouter>
+  <AuthProvider
+    isOpen
+    custom={{ privacyPolicyUrl: 'localhost:3000' }}
+    {...devProps}
+  >
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainScreen/>} />
+        <Route path="/" element={<MainScreen />} />
       </Routes>
-  </BrowserRouter>
+    </BrowserRouter>
   </AuthProvider>
 );
 
